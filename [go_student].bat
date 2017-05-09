36ad3967dcbc3299ce9ab5a95f76827cc0f6332b
Johannes Heucher <coronaconan@gmail.com>
2017-05-09 20:14:14 +0200

    initial example

---
+++ [go_student].bat
@@ -0,0 +1,7 @@
+pdflatex tudkom_students__200910_v1.0.tex
+bibtex tudkom_students__200910_v1.0 >> students.txt
+pdflatex tudkom_students__200910_v1.0.tex
+pdflatex tudkom_students__200910_v1.0.tex
+pdflatex tudkom_students__200910_v1.0.tex
+
+clear.bat
---
+++ clear.bat
@@ -0,0 +1,23 @@
+del *.aux
+del *.toc
+del *.out
+del *.bak
+del *.brf
+del *.blg
+del *.bbl
+del *.lof
+del *.lot
+del *.lol
+del *.dvi
+del *.idx
+del *.ind
+del *.glo
+del *.gls
+del *.nlo
+rem del Chapters\*.aux
+rem del FrontBackMatter\*.aux
+rem del *.ps.gz
+rem del *.ps
+rem del *.txt
+del *.log
+
